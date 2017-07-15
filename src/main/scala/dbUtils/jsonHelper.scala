99fce58b11d4e1859cc61d11e5502103f043541a
vishvAsaH <vishvas.vasuki@gmail.com>
2017-07-14 20:22:11 -0700

    Flexibility in type field name

--- src/main/scala/dbUtils/jsonHelper.scala
+++ src/main/scala/dbUtils/jsonHelper.scala
@@ -6,55 +6,55 @@ import dbSchema.dcs.{DcsBook, DcsChapter, DcsOldBook, DcsSentence}
 import dbSchema.dictionary._
 import dbSchema.grammar.{Praatipadika, Subanta, Subantaavalii, SupVibhakti}
 import org.json4s.native.Serialization
-import org.json4s.{Extraction, ShortTypeHints}
+import org.json4s.{DefaultFormats, Extraction, ShortTypeHints}
 
 import scala.collection.mutable
 
 
-object jsonHelper {
+class JsonHelper {
   val JSON_CLASS_FIELD_NAME = "jsonClass"
-  val formats = Serialization.formats(ShortTypeHints(
-    List(
-      classOf[Text],
-      classOf[QuoteText],
-      classOf[OriginAnnotation],
-      classOf[DescriptionAnnotation],
-      classOf[TopicAnnotation],
-      classOf[RatingAnnotation],
-      classOf[RequestAnnotation],
-      classOf[ReferenceAnnotation],
-      classOf[MemorableBitsAnnotation],
-      classOf[DictEntry],
-      classOf[DictLocation],
-      classOf[DcsBook],
-      classOf[DcsOldBook],
-      classOf[DcsChapter],
-      classOf[DcsSentence],
-      classOf[SupVibhakti],
-      classOf[Subanta],
-      classOf[Subantaavalii],
-      classOf[Praatipadika]
-    )))
+  implicit val formats = new DefaultFormats {
+    override val typeHintFieldName = JSON_CLASS_FIELD_NAME
+    override val typeHints = ShortTypeHints(
+      List(
+        classOf[Text],
+        classOf[QuoteText],
+        classOf[OriginAnnotation],
+        classOf[DescriptionAnnotation],
+        classOf[TopicAnnotation],
+        classOf[RatingAnnotation],
+        classOf[RequestAnnotation],
+        classOf[ReferenceAnnotation],
+        classOf[MemorableBitsAnnotation],
+        classOf[DictEntry],
+        classOf[DictLocation],
+        classOf[DcsBook],
+        classOf[DcsOldBook],
+        classOf[DcsChapter],
+        classOf[DcsSentence],
+        classOf[SupVibhakti],
+        classOf[Subanta],
+        classOf[Subantaavalii],
+        classOf[Praatipadika]
+      ))
+  }.skippingEmptyValues
 
 
   def getJsonMap(caseObj: Any): Map[String,Object] = {
-    implicit val formats = jsonHelper.formats.skippingEmptyValues
     val jobj = Extraction.decompose(caseObj)
     val returnMap = jobj.values.asInstanceOf[Map[String,Object]]
     return returnMap
   }
 
   def asString(caseObj: AnyRef): String = {
-    implicit val formats = jsonHelper.formats.skippingEmptyValues
     Serialization.writePretty(caseObj)
   }
 
   def fromString(jsonStr: String, jsonClassIn: String = null): Any = {
-    implicit val formats = jsonHelper.formats
     var jsonClass = jsonClassIn
     if (jsonClassIn == null) {
       val docMap = Serialization.read[Map[String, _]](jsonStr)
-      jsonClass = docMap.get(jsonHelper.JSON_CLASS_FIELD_NAME).get.asInstanceOf[String]
+      jsonClass = docMap.get(JSON_CLASS_FIELD_NAME).get.asInstanceOf[String]
     }
     //    log debug jsonStr
     jsonClass match {
@@ -81,11 +81,17 @@ object jsonHelper {
   }
 
   def fromJsonMap(jsonMap: mutable.Map[String, _]): Any = {
-    implicit val formats = jsonHelper.formats
     //    log debug (jsonMap.toString)
     val jsonClass = jsonMap.get(JSON_CLASS_FIELD_NAME).get
     val jsonStr = Serialization.writePretty(jsonMap)
     fromString(jsonStr = jsonStr, jsonClassIn = jsonClass.asInstanceOf[String])
   }
+}
 
+object jsonHelper extends JsonHelper {
+
+}
+
+object pyJsonHelper  extends JsonHelper {
+  override val JSON_CLASS_FIELD_NAME = "py/object"
 }
