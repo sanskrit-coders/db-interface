57fe01e9601134bd1bea0bf53eb95e1d816a1888
vishvAsaH <vishvas.vasuki@gmail.com>
2017-07-16 15:55:44 -0700

    Add SCL analysis and tiNanta wrapper.

--- src/main/scala/dbSchema/grammar/Subanta.scala
+++ src/main/scala/dbSchema/grammar/Subanta.scala
@@ -1,8 +1,8 @@
 package dbSchema.grammar
 
+case class Praatipadika(root: String, prakaara: Option[String] = None, linga: Option[String] = None)
 case class SupVibhakti(vibhaktiNum: Int, prakaara: Option[String] = None)
 case class Subanta(pada: String, vibhakti: Option[SupVibhakti] = None,
                    vachana: Option[Int] = None, praatipadika: Option[Praatipadika] = None)
-case class Praatipadika(root: String, prakaara: Option[String] = None, linga: Option[String] = None)
 case class Subantaavalii(praatipadika: Option[Praatipadika] = None, subantaLists: Option[Seq[Seq[Subanta]]])
 
