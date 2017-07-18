fa8b15c93ecc6d1885ee7f9ee0b3097c1ea95405
vishvAsaH <vishvas.vasuki@gmail.com>
2017-07-17 19:31:41 -0700

    Fix bug

--- src/main/scala/dbSchema/vedavaapi/BookPortion.scala
+++ src/main/scala/dbSchema/vedavaapi/BookPortion.scala
@@ -2,4 +2,4 @@ package dbSchema.vedavaapi
 
 case class TextContent(text: Option[String], language: Option[String], encoding: Option[String], var _id: Option[String] = None)
 
-case class BookPortion(title: Option[String], path: Option[String], authors: Option[Seq[String]], base_data: Option[String], portion_class: Option[String], curated_content: Option[String], override targets: Option[Seq[BookPositionTarget]], var _id: Option[String] = None) extends JsonObjectWithTarget(targets = targets)
+case class BookPortion(title: Option[String], path: Option[String], authors: Option[Seq[String]], base_data: Option[String], portion_class: Option[String], curated_content: Option[String], override val targets: Option[Seq[BookPositionTarget]], var _id: Option[String] = None) extends JsonObjectWithTarget(targets = targets)
