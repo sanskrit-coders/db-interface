b78c31a818bf1f67b0bfdb59c810b72b7b0cda94
vishvAsaH <vishvas.vasuki@gmail.com>
2017-09-26 15:58:20 -0700

    Add code to convert archive item to podcast.

--- src/main/scala/dbSchema/archive/ItemInfo.scala
+++ src/main/scala/dbSchema/archive/ItemInfo.scala
@@ -17,7 +17,7 @@ case class ItemInfo(
   def toPodcast(audioFileExtension: String): Podcast = {
     val url = s"https://archive.org/details/${metadata.identifier}"
     val itemFiles = files.filter(fileInfo => fileInfo.name.get.endsWith(s".$audioFileExtension"))
-    val itemPublishTimes = itemFiles.zipWithIndex.map(created.getOrElse(0).toLong + _._2)
+    val itemPublishTimes = itemFiles.zipWithIndex.map(created.getOrElse[Double](0).toLong + _._2)
     var items = itemFiles.zip(itemPublishTimes).map( {case (itemFile: FileInfo, publishTime: Long) => itemFile.toPodcastItem(itemMetadata = metadata, publishTime = publishTime)})
     Podcast(title = metadata.title.getOrElse(metadata.identifier), description = s"A podcast automatically created from the archive item: $url, with description: ${metadata.description.getOrElse("")}",
       website_url = url, publisher = metadata.uploader,
