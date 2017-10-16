3bf54b1d23441c68653e4dd4b45a5eae5c5ec753
vishvAsaH <vishvas.vasuki@gmail.com>
2017-10-15 21:52:41 -0700

    Minor refactoring.

--- src/main/scala/dbSchema/archive/ItemInfo.scala
+++ src/main/scala/dbSchema/archive/ItemInfo.scala
@@ -1,6 +1,6 @@
 package dbSchema.archive
 
-import dbSchema.rss.Podcast
+import dbSchema.rss.{Podcast, PodcastItem}
 
 // Example data: http://jsoneditoronline.org/?id=e031ab3cecf3cd6e0891eb9f303cd963
 case class ItemInfo( created: Option[Double],
@@ -15,20 +15,25 @@ case class ItemInfo( created: Option[Double],
                      workable_servers: Option[List[String]]
                    ) {
 
-  def toPodcast(filePattern: String, publisherEmail: String, imageUrl: String, languageCode: String = "en", useArchiveOrder: Boolean = true, title: Option[String] = None, categories: Seq[String], isExplicitYesNo: Option[String] = None): Podcast = {
-    val url = s"https://archive.org/details/${metadata.identifier}"
+  def getPodcastItems(filePattern: String, useArchiveOrder: Boolean = true, itemPublishTime: Option[Long]): List[PodcastItem] = {
     val itemFiles = files.filter(fileInfo => {
       val fileName = fileInfo.name.get
       fileName.matches(filePattern)
     })
 
-    val itemPublishTime = if (useArchiveOrder) metadata.getModificationTime1970Secs else None
-    var items = itemFiles.zipWithIndex.map( {case (itemFile: FileInfo, index: Int) =>
-       val ordinal = if (useArchiveOrder) Some(index) else None
+    itemFiles.zipWithIndex.map( {case (itemFile: FileInfo, index: Int) =>
+      val ordinal = if (useArchiveOrder) Some(index) else None
       itemFile.toPodcastItem(itemMetadata = metadata, publishTime = itemPublishTime, ordinal = ordinal)})
+  }
 
+  def toPodcast(filePattern: String, publisherEmail: String, imageUrl: String, languageCode: String = "en", useArchiveOrder: Boolean = true, title: Option[String] = None, categories: Seq[String], isExplicitYesNo: Option[String] = None): Podcast = {
+    val url = s"https://archive.org/details/${metadata.identifier}"
     val author = if (metadata.creator.isDefined) metadata.creator else metadata.uploader.map(_.replaceFirst("@.+", ""))
+    val itemPublishTime = if (useArchiveOrder) metadata.getModificationTime1970Secs() else None
+
     val titleFinal = if (title.isDefined) title.get else metadata.title.getOrElse(metadata.identifier)
+
+    val items = getPodcastItems(filePattern=filePattern, useArchiveOrder = useArchiveOrder, itemPublishTime = itemPublishTime)
 
     // Archive items seem to be available under variants of the Creative Commons license, chosen at upload-time. (Deduced from seeing https://archive.org/editxml.php?type=audio&edit_item=CDAC-tArkShya-shAstra-viShayaka-bhAShaNAni )
     Podcast(title = titleFinal, description = s"A podcast created using https://github.com/vedavaapi/scala-akka-http-server from the archive item: $url, with description:\n${metadata.description.getOrElse("")}.",
